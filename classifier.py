551ec5bc7fa3b3730d8837df245d3c30810d9e38
Eyosyas Dagnachew <eyosyaswd@outlook.com>
2019-03-27 19:06:06 -0400

    Added some comments and notes.

--- classifier.py
+++ classifier.py
@@ -8,15 +8,15 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 
-def train(x_train, y_train):
+def train(x_train, y_train, x_test, y_test):
 	print("Initializing classifier...\n")
 	vectorizer = TfidfVectorizer()
 	svm_classifier = svm.LinearSVC(C=0.1)
 	svm_pipeline = Pipeline([("vectorizer", vectorizer), ("linear_svc", svm_classifier)])
-	
+
 	print("Training...\n")
 	svm_pipeline.fit(x_train, y_train)
 
 	print("Predicting...\n")
-	svm_pipeline.predict(x_train)
-	print(svm_pipeline.score(x_train, y_train))
+	# svm_pipeline.predict(x_train)
+	print(svm_pipeline.score(x_test, y_test))
--- main.py
+++ main.py
@@ -15,18 +15,25 @@ def debug(some_str):
 
 
 def split_data(tweets_df):
+	""" Splits the dataset into training and testing data. """
 	tweets = tweets_df["text"].tolist()
 	polarities = tweets_df["polarity"].tolist()
-	x_train, x_test, y_train, y_test = train_test_split(tweets, polarities, test_size=.20, random_state=42)
+	x_train, x_test, y_train, y_test = train_test_split(tweets, polarities, test_size=.25, random_state=42)
 	return x_train, x_test, y_train, y_test
 
 
 def main():
+	# read labelled csv and covert it to a pandas dataframe
 	tweets_df = pd.read_csv("data/labelled-dataset/100-example-labelled-tweets.csv")
+
+	# conduct preprocessing
 	preprocessor.preprocess_df(tweets_df)
-	# print(tweets_df["polarity"].tolist())
+
+	# split the dataset into training and testing data
 	x_train, x_test, y_train, y_test = split_data(tweets_df)
-	classifier.train(x_train, y_train)
+
+	# create a classifier and train it using the dataset
+	classifier.train(x_train, y_train, x_test, y_test)
 
 if __name__ == '__main__':
 	main()
